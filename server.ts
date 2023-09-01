38de58f95d0a1f233fe4cdae932f95b08cd13764
Max Duval <max.duval@theguardian.com>
2023-09-01 12:22:42 +0100

    feat: serve slides
    - small server to present slides
    - unified to parse markdown to HTML
    - code highlights

---
+++ server.ts
@@ -0,0 +1,85 @@
+import { readFile, readdir } from 'node:fs/promises';
+import { RequestListener, createServer } from 'node:http';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+import remarkGfm from 'remark-gfm';
+import rehypeHighlight from 'rehype-highlight';
+
+const host = 'localhost';
+const port = 8000;
+const STARTS_WITH_DIGIT = /^\d+-/;
+
+const session = async (id: string) => {
+	try {
+		const slides = await readFile(`.${id}/slides.md`, 'utf-8').then(html);
+		return slides;
+	} catch (error) {
+		return `<p>Could not find <code>${id}/slides.md</code>. <a href="/">Go back</a>.<p>`;
+	}
+};
+
+const processor = unified()
+	.use(remarkParse)
+	.use(remarkGfm)
+	.use(remarkRehype)
+	.use(rehypeHighlight)
+	.use(rehypeStringify);
+
+/** Markdown in, HTML out */
+const html = (markdown: string) =>
+	Promise.all(
+		markdown.split('\n---\n').map((slide) => processor.process(slide)),
+	).then((slides) =>
+		slides
+			.map(
+				(slide, index) =>
+					`<article>${slide}<aside>${index + 1}/${
+						slides.length
+					}</aside></article>`,
+			)
+			.join('\n'),
+	);
+
+const requestListener: RequestListener = async (request, response) => {
+	const url = new URL(request.url ?? '/', `http://${request.headers.host}`);
+
+	const template = await readFile('./assets/template.html', 'utf-8');
+	const styles = await readFile('./assets/styles.css', 'utf-8');
+
+	switch (url.pathname) {
+		case '/': {
+			response.end(
+				template.replace('<!-- TITLE -->', 'TypeScript School').replace(
+					'<!-- MAIN -->',
+					`<ol>${(await readdir('.'))
+						.filter((name) => name.match(STARTS_WITH_DIGIT))
+						.map(
+							(session) =>
+								`<li><a href="/${session}">${session.replace(
+									STARTS_WITH_DIGIT,
+									'',
+								)}</a></li>`,
+						)
+						.join('\n')}</ol>`,
+				),
+			);
+			return;
+		}
+		default: {
+			response.end(
+				template
+					.replace('<!-- TITLE -->', 'TypeScript School')
+					.replace('<!-- STYLES -->', `<style>${styles}</style>`)
+					.replace('<!-- MAIN -->', await session(url.pathname)),
+			);
+			return;
+		}
+	}
+};
+
+const server = createServer(requestListener);
+server.listen(port, host, () => {
+	console.log(`See the slides on http://${host}:${port}`);
+});
